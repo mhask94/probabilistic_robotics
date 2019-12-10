b91eaab8b761591050fdb8c272106317bb37a3cb
Mat Haskell <mhaskell9@gmail.com>
2019-12-09 20:49:46 -0700

    Finished adding simulator

--- pomdp_planning/main.py
+++ pomdp_planning/main.py
@@ -1,12 +1,12 @@
 import numpy as np
-from itertools import permutations as perms
 import matplotlib.pyplot as plt
 from pomdp import POMDP
+from simulator import Simulator
 
 time_horizon = 20
 
-pz_true = 0.7
-pu3_true = 0.8
+pz = 0.7
+pu3 = 0.8
 
 lava_cost_f = -100
 lava_cost_b = -50 
@@ -15,18 +15,20 @@ u3_cost = -1
 
 samples = 1000
 
-planner = POMDP(pz_true, pu3_true, lava_cost_f, lava_cost_b, door_cost, u3_cost,
-        samples, prune=True)
+planner = POMDP(pz, pu3, lava_cost_f, lava_cost_b, door_cost, u3_cost, samples, 
+        prune=True)
 
 for T in range(time_horizon):
     obs = planner.observe()
     pred = planner.predict()
 
-print(pred)
+#print(pred)
 
-book_lines = np.array([[100, -100],[-50, 100], [-1, -1]])
+book_lines = np.array([[door_cost, lava_cost_f],[lava_cost_b, door_cost]])
 plt.figure()
-plt.plot([0, 1], book_lines.T, 'r:')
+plt.plot([0, 1], book_lines.T, 'r--')
+plt.plot([planner.prob_turn_start, planner.prob_turn_start], [-500, 500], 'g:')
+plt.plot([planner.prob_turn_end, planner.prob_turn_end], [-500, 500], 'g:')
 plt.plot(planner.prob[0], planner.value_fn, 'b')
 plt.title('Value Function')
 plt.xlabel('Probability of $x_1$')
@@ -37,8 +39,11 @@ plt.pause(0.00001)
 ### simulate 10 times
 wins = 0
 for T in range(10):
-    sim = Simulator(planner.prob_turn_start, planner.prob_turn_end)
+    sim = Simulator(planner.prob_turn_start, planner.prob_turn_end, pz, pu3, 
+            lava_cost_f, lava_cost_b, door_cost, u3_cost)
     wins += sim.run()
+    print('Sim {}: {}\tactions:{}\treward: {}'.format(T, sim.msg, sim.num_turns,
+        sim.reward))
 print('Won {}/10 times'.format(wins))
 
 plt.show()
--- pomdp_planning/simulator.py
+++ pomdp_planning/simulator.py
@@ -2,18 +2,62 @@ import numpy as np
 from numpy.random import rand
 
 class Simulator():
-    def __init__(self, prob1, prob2, pz_prob, pu3_prob):
+    def __init__(self, prob1, prob2, pz, pu3, lava_f, lava_b, door, u3_cost):
         self.prob1, self.prob2 = prob1, prob2
         self.state = 1      # face lava initially
         self.belief = 0.6   # initial p1
-        self.pz_prob = pz_prob
-        self.pu3_prob = pu3_prob
+        self.pz = pz
+        self.pu3 = pu3
+        self.running = True
+        self.num_turns = 0
+        self.reward = 0
+        self.lava_f = lava_f
+        self.lava_b = lava_b
+        self.door = door
+        self.u3_cost = u3_cost
 
     def run(self):
-        self.observe()
-        self.act()
+        while self.running:
+            self.observe()
+            self.act()
+        return self.win
 
     def observe(self):
-        z = rand() < self.pz_prob
+        z_prob = rand() < self.pz
+        if z_prob: z = self.state
+        else: z = 1 - self.state
         if z:
-            self.belief = self.pz_prob*self.belief / ()
+            self.belief = self.pz * self.belief / ((2*self.pz - 1) * \
+                    self.belief + (1-self.pz))
+        else:
+            self.belief = (1-self.pz) * self.belief / (self.pz - \
+                    (2*self.pz - 1) * self.belief)
+
+    def act(self):
+        if self.belief < self.prob1: # forward -> u1
+            if self.state:
+                self.msg = 'Drove forwards into LAVA :('
+                self.win = False
+                self.reward += self.lava_f
+            else:
+                self.msg = 'Drove forwards into GOAL :)'
+                self.win = True
+                self.reward += self.door
+            self.running = False
+        elif self.belief > self.prob2: # backward -> u2
+            if self.state:
+                self.msg = 'Drove backward into GOAL :)'
+                self.win = True
+                self.reward += self.door
+            else:
+                self.msg = 'Drove backward into LAVA :('
+                self.win = False
+                self.reward += self.lava_b
+            self.running = False
+        else: # turn -> u3
+            turn = rand() < self.pu3
+            self.belief = self.pu3 - (2*self.pu3 - 1)*self.belief
+            if turn:
+                self.state = 1 - self.state
+            self.num_turns += 1
+            self.reward += self.u3_cost
